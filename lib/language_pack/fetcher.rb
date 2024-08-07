057968a31bffe2d53a040bf6ddaec2e44dba72fc
akuriako <akuriako@cisco.com>
2024-08-07 23:01:05 +0530

    correct syntax

--- lib/language_pack/fetcher.rb
+++ lib/language_pack/fetcher.rb
@@ -21,12 +21,14 @@ module LanguagePack
 
     def fetch_untar(path, files_to_extract = nil)
       curl = curl_command("#{@host_url.join(path)} -s -o")
-      run!("#{curl} - | tar zxf - #{files_to_extract}", error_class: FetchError)
+      run!("#{curl} | tar zxf - #{files_to_extract}", error_class: FetchError)
+      print "done - fetch_untar"
     end
 
     def fetch_bunzip2(path, files_to_extract = nil)
       curl = curl_command("#{@host_url.join(path)} -s -o")
-      run!("#{curl} - | tar jxf - #{files_to_extract}", error_class: FetchError)
+      run!("#{curl} | tar jxf - #{files_to_extract}", error_class: FetchError)
+      print "done - fetch_bunzip2"
     end
 
     private
