11c962c71dc2c1be7ff20f1e60d4f59af951b9ca
Naman Garg <namangar@cisco.com>
2025-07-28 16:00:01 +0530

    Chnaged ruby version

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -16,7 +16,7 @@ class LanguagePack::Base
   include LanguagePack::ShellHelpers
 
   VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby"
-  DEFAULT_LEGACY_STACK = "cedar"
+  DEFAULT_LEGACY_STACK = "heroku"
   ROOT_DIR             = File.expand_path("../../..", __FILE__)
 
   attr_reader :build_path, :cache
